023c7072f957e3fdc15b7621c9fae74dfaf87317
Edwin Flores Vargas <edwinfloresvargas.dev@gmail.com>
2026-04-18 13:15:06 -0600

    Añadida configuración de despliegue en EC2 para ambientes DEV y PROD, incluyendo manejo de claves SSH y actualización de la aplicación. También se creó un nuevo archivo de migración para agregar contraseña a alumnos.

--- db/database.py
+++ db/database.py
@@ -1,16 +1,25 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from config.settings import Settings
 
-settings = Settings()
+# 🔥 REEMPLAZA TU PASSWORD Y DB
+DATABASE_URL = "mysql+pymysql://admin:db_examen@examen-backend-db.cd084u2s2kv7.us-east-2.rds.amazonaws.com:3306/examen"
 
-engine = create_engine(settings.DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,  # 🔥 evita errores de conexión
+    connect_args={
+        "ssl": {
+            "ca": "global-bundle.pem"  # 🔥 certificado AWS
+        }
+    }
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-# 🔥 Dependencia para FastAPI
+
+# 🔥 dependencia FastAPI
 def get_db():
     db = SessionLocal()
     try:
